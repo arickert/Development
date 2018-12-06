e0b3d4d21b1bba3487a855911f32115b5d680859
Andy Rickert <andyrickert@andys-mbp.devices.brown.edu>
2018-12-05 20:23:30 -0500

    Updates

---
+++ precache-manifest.5e243cfc28ce5d322534958e52f4f282.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f7db6f413f02ac1b4d3d27fd7d01f59",
+    "url": "/Development/static/media/6.3f7db6f4.jpg"
+  },
+  {
+    "revision": "27a9edf05a222042e98e",
+    "url": "/Development/static/css/main.b7cf5f2f.chunk.css"
+  },
+  {
+    "revision": "5ae957c373f79cae09dd",
+    "url": "/Development/static/js/1.5ae957c3.chunk.js"
+  },
+  {
+    "revision": "ee665625b694b0ce5b45",
+    "url": "/Development/static/js/runtime~main.ee665625.js"
+  },
+  {
+    "revision": "a0fdbddc3408749613fd38e4e32f2344",
+    "url": "/Development/static/media/1.a0fdbddc.jpg"
+  },
+  {
+    "revision": "8d8faaaf4cbf3ea118c62d5d887b880a",
+    "url": "/Development/static/media/2.8d8faaaf.jpg"
+  },
+  {
+    "revision": "13628684d755ac24e7af401c6bb1e798",
+    "url": "/Development/static/media/3.13628684.jpg"
+  },
+  {
+    "revision": "ca85a0c3eadb3e50fcec11a2a3109fbe",
+    "url": "/Development/static/media/4.ca85a0c3.jpg"
+  },
+  {
+    "revision": "b13431af7bb48423aa37649dc4572b91",
+    "url": "/Development/static/media/5.b13431af.jpg"
+  },
+  {
+    "revision": "27a9edf05a222042e98e",
+    "url": "/Development/static/js/main.27a9edf0.chunk.js"
+  },
+  {
+    "revision": "374603674ffe4195c5a9306a341dd82b",
+    "url": "/Development/static/media/7.37460367.jpg"
+  },
+  {
+    "revision": "bf29bde325887cdc8c49ae30dbe834c6",
+    "url": "/Development/static/media/8.bf29bde3.jpg"
+  },
+  {
+    "revision": "a1f078caaa2023954c8a68a5d064feb4",
+    "url": "/Development/static/media/9.a1f078ca.jpg"
+  },
+  {
+    "revision": "7d8f2b863130bbb0a329e7f8df2b6fc0",
+    "url": "/Development/static/media/10.7d8f2b86.jpg"
+  },
+  {
+    "revision": "86b601fa5968e62f82ee16ed7447b462",
+    "url": "/Development/static/media/11.86b601fa.jpg"
+  },
+  {
+    "revision": "10ff2256eb11298bd034ef8cc7eac4d6",
+    "url": "/Development/static/media/12.10ff2256.jpg"
+  },
+  {
+    "revision": "62cd0df876366f3280086dabd4866358",
+    "url": "/Development/static/media/anti.62cd0df8.PNG"
+  },
+  {
+    "revision": "fe20f819cdc97fd286a2f16b2504ca0e",
+    "url": "/Development/index.html"
+  }
+];
